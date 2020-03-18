5735a701cf72017b81d15b95968d09a1c398748d
hugo donkerbroek <lolxbroekje@gmail.com>
2020-03-18 20:24:21 +0100

    updated merged files, added contact page, removed img from css folder

--- src/main/java/nl/bioinf/fooddiary/config/WebSecurityConfig.java
+++ src/main/java/nl/bioinf/fooddiary/config/WebSecurityConfig.java
@@ -40,7 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers( "/", "/home", "/*/home", "/images/**", "/css/**", "/contact").permitAll()
+                .antMatchers( "/", "/home", "/*/home", "/images/**", "/css/**", "/contact", "/*/contact").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
--- src/main/java/nl/bioinf/fooddiary/control/ContactController.java
+++ src/main/java/nl/bioinf/fooddiary/control/ContactController.java
@@ -5,12 +5,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Locale;
+
 @Controller
 public class ContactController {
 
-    @RequestMapping(
-            value = {"/contact"}, method = RequestMethod.GET)
-    public String home(Model model) {
+    @RequestMapping(value = {"/contact"}, method = RequestMethod.GET)
+    public String contactWithoutLocale(Locale locale, Model model) {
+        model.addAttribute("page_name", "contact");
+        return "redirect:" + locale.getLanguage() + "/contact";
+    }
+
+    @RequestMapping(value = "/{locale}/contact")
+    public String  contactWithLocale(Model model) {
         model.addAttribute("page_name", "contact");
         return "contact";
     }
