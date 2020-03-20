ede0db288ed680647373bb5c8fc55de0de4d726c
Piet Puk <p.puk@gmail.com>
2020-03-20 23:35:26 +0100

    Moved the csv files to a different location, Almost finished the ProductCsvParser, todo: add JUnit testing

--- src/main/java/nl/bioinf/fooddiary/model/csvparser/ProductCsvParser.java
+++ src/main/java/nl/bioinf/fooddiary/model/csvparser/ProductCsvParser.java
@@ -1,10 +1,12 @@
 package nl.bioinf.fooddiary.model.csvparser;
 
 import com.opencsv.CSVReader;
+import nl.bioinf.fooddiary.model.nutrient.NutrientValues;
 import nl.bioinf.fooddiary.model.product.*;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * @author Tom Wagenaar
@@ -27,7 +29,7 @@ public class ProductCsvParser {
      * Catch: IOException, this could be a FileNotFoundException whenever the file isn't found.
      */
     public void readCsvFile() {
-        String csvFile = "src/main/resources/csv/nevo_online_2019_Product.csv";
+        String csvFile = "src/data/csv/nevo_online_2019_Product.csv";
 
         // try to read every line in the csv file and pass each line individually on to the parseCsvFile() method
         try {
@@ -52,36 +54,45 @@ public class ProductCsvParser {
 
     /**
      * This method receives every line in the nevo_online_2019_Product.csv file separately and groups the columns
-     * into groups for more readability and passes it on to the respective class.
+     * into groups for more readability and passes it on to classes that represent those groups, those classes store
+     * the information and return objects. These objects are then passed on to the Product class that stores all
+     * the information for a single product in the Nevo_Online_2019 file.
      *
      * @param line, represents a single line and therefore a single product in the nevo_online_2019_Product.csv file.
      */
-    public void parseCsvFile(String[] line) {
+    public Product parseCsvFile(String[] line) {
         int code = Integer.parseInt(line[2]);
 
+        // Parse the group code and group description into an object.
         ProductGroup productGroup = ProductGroup.builder(Integer.parseInt(line[0]), line[1])
                 .build();
 
+        // Parse the descriptionDutch, descriptionEnglish and synonymous of the product into an object.
         ProductDescription productDescription = ProductDescription.builder(line[3], line[4])
                 .synonymous(line[5])
                 .build();
 
+        // Parse the measurement -Unit, -Quantity and -Comment into an object.
         ProductMeasurement productMeasurement = ProductMeasurement.builder(line[6], Integer.parseInt(line[7]))
                 .measurementComment(line[8])
                 .build();
 
+        // Parse the enrichedWith and tracesOf values from a the product into an object.
         ProductInfoExtra productInfoExtra = ProductInfoExtra.builder()
                 .enrichedWith(line[9])
                 .tracesOf(line[10])
                 .build();
 
-        Product product = new Product(code, productGroup, productDescription, productMeasurement, productInfoExtra);
-        System.out.println(product.getProductInfoExtra().toString());
 
-    }
+        // Parse all nutrient values from a single product line into one object.
+        NutrientValues nutrientValues = NutrientValues.builder()
+                .nutrientValue(Arrays.copyOfRange(line, 11, line.length))
+                .build();
 
-    public static void main(String[] args) {
-        ProductCsvParser csvParser = new ProductCsvParser();
-        csvParser.readCsvFile();
+        // Pass on all the objects on to the Product class, this class stores all the objects into one single object.
+        Product product = new Product(code, productGroup, productDescription, productMeasurement,
+                productInfoExtra, nutrientValues);
+
+        return product;
     }
 }
