d30f55f0f86ed8ea6541f8f7e919a59eb11b644f
Piet Puk <p.puk@gmail.com>
2020-03-23 12:59:13 +0100

    Made a small test for the parseCsvFile method

--- src/test/java/nl/bioinf/fooddiary/model/csvparser/ProductCsvParserTest.java
+++ src/test/java/nl/bioinf/fooddiary/model/csvparser/ProductCsvParserTest.java
@@ -1,5 +1,7 @@
 package nl.bioinf.fooddiary.model.csvparser;
 
+import nl.bioinf.fooddiary.model.nutrient.NutrientValues;
+import nl.bioinf.fooddiary.model.product.Product;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,7 +16,37 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProductCsvParserTest {
 
     @Test
-    void readCsvFileSunny() {
+    void parseCsvFileSunny() {
+        String[] testLine = {"1","Aardappelen en knolgewassen","1","Aardappelen rauw","Potatoes raw","","g","100",
+                            "Per 100g.","","","371","88","2",""};
+
+        ProductCsvParser parser = new ProductCsvParser();
+        Product product = parser.parseCsvFile(testLine);
+
+        int actualCode = product.getCode();
+        int expectedCode = 1;
+
+        String actualProductDescriptionEnglish = product.getProductDescription().getDescriptionEnglish();
+        String expectedProductDescriptionEnglish = "Potatoes raw";
+
+        String actualNutrientValue = product.getNutrientValues().getNutrients().get(0).getValue();
+        String expectedNutrientValue = "371";
+
+        String actualNutrientValue2 = product.getNutrientValues().getNutrients().get(3).getValue();
+        String expectedNutrientValue2 = "_NO_VALUE_";
+
+        assertEquals(expectedCode, actualCode);
+        assertEquals(expectedProductDescriptionEnglish, actualProductDescriptionEnglish);
+        assertEquals(expectedNutrientValue, actualNutrientValue);
+        assertEquals(expectedNutrientValue2, actualNutrientValue2);
+    }
+
+
+    @Test
+    void parseCsvFileNullInput() {
+        String[] testLine = {null,"Aardappelen en knolgewassen","4","Aardappelen rauw","Potatoes raw","","g","100",
+                "Per 100g.","","","371","88","2"};
 
     }
+
 }
