f4064c823776d32629e010c96239ff5f2bcb1cc8
Piet Puk <p.puk@gmail.com>
2020-03-23 13:00:34 +0100

    Final version

--- src/main/java/nl/bioinf/fooddiary/model/product/ProductGroup.java
+++ src/main/java/nl/bioinf/fooddiary/model/product/ProductGroup.java
@@ -93,7 +93,6 @@ public final class ProductGroup {
      */
     public static class ProductGroupBuilder {
         // Required parameters
-        @NotNull
         @Min(1) @Max(999)
         private final int groupCode;
 
