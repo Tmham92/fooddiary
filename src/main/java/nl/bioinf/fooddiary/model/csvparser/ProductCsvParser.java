4a70a727f315ac322d76fc9bd5172a2ac626cc80
Piet Puk <p.puk@gmail.com>
2020-03-23 12:08:54 +0100

    Made a few minor changes to the javadoc, comments and code.

--- src/main/java/nl/bioinf/fooddiary/model/csvparser/ProductCsvParser.java
+++ src/main/java/nl/bioinf/fooddiary/model/csvparser/ProductCsvParser.java
@@ -6,11 +6,13 @@ import nl.bioinf.fooddiary.model.product.*;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Tom Wagenaar
- * @version 0.0.1
+ * @version 0.0.3
  * date: 18-03-2020
  *
  * Parses the nevo_online_2019_Product.csv file, this file consists out of products, a single product in the table
@@ -23,12 +25,13 @@ public class ProductCsvParser {
 
     /**
      * Read a csv file, in this case the nevo_online_2019_Product.csv file. Pass every line separately on to the
-     * parseCsvFile() method. An stack trace of the IOException is shown whenever the csv file isn't found or readable,
-     * then stop the system.
+     * parseCsvFile() method. This method then returns a Product object and that object is then added to a lsit. An
+     * stack trace of the IOException is shown whenever the csv file isn't found or readable, then stop the system.
      *
      * Catch: IOException, this could be a FileNotFoundException whenever the file isn't found.
      */
-    public void readCsvFile() {
+    public List<Product> readCsvFile() {
+        List<Product> productList = new ArrayList<>();
         String csvFile = "src/data/csv/nevo_online_2019_Product.csv";
 
         // try to read every line in the csv file and pass each line individually on to the parseCsvFile() method
@@ -44,19 +47,21 @@ public class ProductCsvParser {
                     iteration++;
                     continue;
                 }
-                parseCsvFile(line);
+                productList.add(parseCsvFile(line));
             }
         } catch (IOException exception) {
             exception.printStackTrace();
             System.exit(0);
         }
+
+        return productList;
     }
 
     /**
      * This method receives every line in the nevo_online_2019_Product.csv file separately and groups the columns
      * into groups for more readability and passes it on to classes that represent those groups, those classes store
      * the information and return objects. These objects are then passed on to the Product class that stores all
-     * the information for a single product in the Nevo_Online_2019 file.
+     * the information for a single product from the Nevo_Online_2019 file.
      *
      * @param line, represents a single line and therefore a single product in the nevo_online_2019_Product.csv file.
      */
@@ -64,7 +69,7 @@ public class ProductCsvParser {
         int code = Integer.parseInt(line[2]);
 
         // Parse the group code and group description into an object.
-        ProductGroup productGroup = ProductGroup.builder(Integer.parseInt(line[0]), line[1])
+        ProductGroup productGroup = ProductGroup.builder(line[0], line[1])
                 .build();
 
         // Parse the descriptionDutch, descriptionEnglish and synonymous of the product into an object.
