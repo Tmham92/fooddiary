e2dc27cb43f6233d0c49b54d485f56708e8c2b30
Tom Wagenaar <p.puk@gmail.com>
2020-03-26 21:21:01 +0100

    Test methods created, contains an error

---
+++ src/test/java/nl/bioinf/fooddiary/dao/productnutrient/ProductNutrientRepositoryTest.java
@@ -0,0 +1,43 @@
+package nl.bioinf.fooddiary.dao.productnutrient;
+
+import nl.bioinf.fooddiary.model.nutrient.ProductNutrient;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class ProductNutrientRepositoryTest {
+    // Instance variable declaration
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private ProductNutrientDAO productNutrientDAO;
+
+    @Before
+    public void setUp() {
+        // given
+        ProductNutrient productNutrientObject = ProductNutrient.builder(10, "ENERG", "0,256")
+                .build();
+
+
+        entityManager.persist(productNutrientObject);
+    }
+
+    @Test
+    public void getProductNutrientById_ReturnID() {
+        // When
+        ProductNutrient productNutrient = productNutrientDAO.getProductNutrientById(10);
+
+        //then
+        assertEquals(productNutrient.getNutrientValue(), "0,256");
+    }
+}
