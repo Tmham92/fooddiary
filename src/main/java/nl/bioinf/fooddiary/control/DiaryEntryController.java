e29fa5ce6555d954208d7cd0c53cbf7056ca8eb9
Hans96 <hanszijlstra96@gmail.com>
2020-03-27 16:24:25 +0100

    created diary-entry page

--- src/main/java/nl/bioinf/fooddiary/control/DiaryEntryController.java
+++ src/main/java/nl/bioinf/fooddiary/control/DiaryEntryController.java
@@ -1,7 +1,33 @@
 package nl.bioinf.fooddiary.control;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+import java.util.Locale;
+
 /**
  * @author Hans Zijlstra
  */
+
+@Controller
 public class DiaryEntryController {
-}
+
+    @RequestMapping(value = "/diary-entry")
+        public String diaryWithoutLocale(Locale locale) {
+        return "redirect:" + locale.getLanguage() + "/diary-entry";
+
+        }
+
+    @RequestMapping(value = "/{locale}/diary-entry")
+    public String  diaryWithLocale(HttpServletRequest request, Authentication authentication, Model model, Principal principal) {
+
+        return "/diary-entry";
+    }
+
+    }
