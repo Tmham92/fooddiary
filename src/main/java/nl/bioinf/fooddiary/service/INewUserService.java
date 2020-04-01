279442f5ecb7591bedefa6513f484cd784b631eb
hugo donkerbroek <lolxbroekje@gmail.com>
2020-04-01 15:47:28 +0200

    database classes and interfaces created, user object for form creation created

--- src/main/java/nl/bioinf/fooddiary/service/INewUserService.java
+++ src/main/java/nl/bioinf/fooddiary/service/INewUserService.java
@@ -1,4 +1,12 @@
 package nl.bioinf.fooddiary.service;
 
+import nl.bioinf.fooddiary.model.newuser.NewUser;
+
+/**
+ * @author Hugo Donkerbroek
+ */
+
+
 public interface INewUserService {
+    void addNewUser(NewUser newUser);
 }
--- src/main/java/nl/bioinf/fooddiary/service/NewUserService.java
+++ src/main/java/nl/bioinf/fooddiary/service/NewUserService.java
@@ -1,4 +1,22 @@
 package nl.bioinf.fooddiary.service;
 
-public class NewUserService {
+import nl.bioinf.fooddiary.dao.newuserdao.NewUserDAO;
+import nl.bioinf.fooddiary.model.newuser.NewUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Hugo Donkerbroek
+ */
+
+@Service
+public class NewUserService implements INewUserService {
+
+    @Autowired
+    private NewUserDAO newUserDAO;
+
+    @Override
+    public void addNewUser(NewUser newUser) {
+        newUserDAO.addNewUser(newUser);
+    }
 }
