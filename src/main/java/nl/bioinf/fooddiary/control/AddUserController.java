8d707674f31dfa1f080c80f06a9bfd232323f066
hugo donkerbroek <lolxbroekje@gmail.com>
2020-04-01 15:54:45 +0200

    added author notation

--- src/main/java/nl/bioinf/fooddiary/control/AddUserController.java
+++ src/main/java/nl/bioinf/fooddiary/control/AddUserController.java
@@ -12,6 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.Locale;
 
+/**
+ * @author Hugo Donkerbroek
+ */
+
 @Controller
 public class AddUserController {
 
