0b5ed29f4015f23e3cb4944f724c19fd3f08f5e8
Tom Wagenaar <p.puk@gmail.com>
2020-04-17 19:09:34 +0200

    Added javadoc

--- src/main/java/nl/bioinf/fooddiary/control/DataController.java
+++ src/main/java/nl/bioinf/fooddiary/control/DataController.java
@@ -1,9 +1,9 @@
 package nl.bioinf.fooddiary.control;
 
 import nl.bioinf.fooddiary.FooddiaryApplication;
-import nl.bioinf.fooddiary.dao.nutrient.NutrientRepository;
-import nl.bioinf.fooddiary.dao.product.ProductRepository;
-import nl.bioinf.fooddiary.dao.productnutrient.ProductNutrientRepository;
+import nl.bioinf.fooddiary.dao.NutrientRepository;
+import nl.bioinf.fooddiary.dao.ProductRepository;
+import nl.bioinf.fooddiary.dao.ProductNutrientRepository;
 import nl.bioinf.fooddiary.model.PrepareDataInjection;
 import nl.bioinf.fooddiary.model.csvparser.NutrientCsvParser;
 import nl.bioinf.fooddiary.model.csvparser.ProductCsvParser;
@@ -14,15 +14,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-
+/**
+ * @author Tom Wagenaar
+ *
+ * Data controller that serves the /data url, when called the data from the parsers is inserted into the database.
+ * For this process the repositories interfaces from the DAO's are used to create sql queries that support inserting
+ * the data.
+ */
 @Controller
 public class DataController {
 
+    // Logger object used for logging the process.
     private static final Logger logger = LoggerFactory.getLogger(FooddiaryApplication.class);
 
     @Autowired
@@ -38,12 +44,20 @@ public class DataController {
     private List<Nutrient> nutrientList = new NutrientCsvParser().readCsvFile();
     private List<List<ProductNutrient>> productNutrientData = new PrepareDataInjection().startRearrangeData();
 
+    /**
+     * Method that uses the productRepository, nutrientRepository and Product_Nutrient repository. These repositories
+     * are used to call the methods in the DAO respectively to those repositories for inserting data from the
+     * productCsvParser, NutrientCsvParser and productNutrientData classes.
+     * @return
+     */
     @RequestMapping(value = "/data")
-    public String insertProductData(Model model) {
-        logger.info("/data url has been called, showing data for testing.");
+    public String insertNevoData() {
+        logger.info("/data url has been called, inserting data into the database.");
+
 
         logger.info("Inserting product data into the product table.");
         for (Product product:productList) {
+
             productRepository.insertProductData(product);
         }
         logger.info("Inserted product data into the product table");
