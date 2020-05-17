fc8d3d2171edd74d0e2df093b38dac92043a568c
hugo donkerbroek <lolxbroekje@gmail.com>
2020-05-17 17:15:00 +0200

    project creation interface, service, model and database connection classes

--- src/main/java/nl/bioinf/fooddiary/model/project/NewProject.java
+++ src/main/java/nl/bioinf/fooddiary/model/project/NewProject.java
@@ -1,4 +1,50 @@
 package nl.bioinf.fooddiary.model.project;
 
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.Date;
+
 public class NewProject {
+    private int id;
+    @NotNull
+    private String name;
+    @NotNull
+    private String project_leader;
+    private Date closing_date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProject_leader() {
+        return project_leader;
+    }
+
+    public void setProject_leader(String project_leader) {
+        this.project_leader = project_leader;
+    }
+
+    public LocalDate getCreation_date() {
+        return LocalDate.now();
+    }
+
+    public Date getClosing_date() {
+        return closing_date;
+    }
+
+    public void setClosing_date(Date closing_date) {
+        this.closing_date = closing_date;
+    }
 }
--- src/main/java/nl/bioinf/fooddiary/service/NewProjectService.java
+++ src/main/java/nl/bioinf/fooddiary/service/NewProjectService.java
@@ -1,4 +1,18 @@
 package nl.bioinf.fooddiary.service;
 
-public class NewProjectService {
+import nl.bioinf.fooddiary.dao.jdbc.NewProjectDAO;
+import nl.bioinf.fooddiary.model.project.NewProject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NewProjectService implements INewProjectService{
+
+    @Autowired
+    private NewProjectDAO newProjectDAO;
+
+    @Override
+    public void addNewProject(NewProject newProject) {
+        newProjectDAO.addNewProject(newProject);
+    }
 }
