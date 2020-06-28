758dfc80f80ad535258b706645fcbb230032124f
hugo donkerbroek <lolxbroekje@gmail.com>
2020-06-28 21:36:22 +0200

    Removed redundant interface

--- src/main/java/nl/bioinf/fooddiary/dao/jdbc/NewProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package nl.bioinf.fooddiary.dao.jdbc;
-
-import nl.bioinf.fooddiary.model.project.NewProject;
-
-public interface NewProjectRepository {
-    void addNewProject(NewProject newProject);
-}
